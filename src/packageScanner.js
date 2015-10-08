3bf5d97dbefcb83f83f27ebff28c2e53f0985e63
Tim Roes <mail@timroes.de>
2015-10-08 15:05:51 +0200

    docs: fix docs for packageScanner

--- src/packageScanner.js
+++ src/packageScanner.js
@@ -6,25 +6,17 @@ var directories = require('./directories');
  * Returns an array of sorted dependency informations.
  * The sorting will be influenced by the "order" key in the config file.
  *
- * @return {object[]} an array of objects with each object holding the id (id)
- *                    and the module directory (dir) of the dependency
+ * @param {Config} config - the config object
+ * @return {string[]} an array of the ids of all dependencies this library
+ *                    needs to handle
  */
 module.exports.getDependencies = function(config) {
 	// get the bower.json of the project using this library
 	var bowerJson = require(directories.calling('bower.json'));
 
-	// var pkgs = [];
 	return Object.keys(bowerJson.dependencies).filter(function(id) {
 		return !config.isExcluded(id);
 	});
-	// for(var pkgId in bowerJson.dependencies) {
-	// 	if (!this.config.isExcluded(pkgId)) {
-	// 		pkgs.push({
-	// 			id: pkgId,
-	// 			dir: directories.modules(pkgId)
-	// 		});
-	// 	}
-	// }
 
 	return pkgs;
 };
